a2ba32ce5ddb229a81b33d1e2606832b8ae93040
Maksim Tyazhelkov <mtyazhelkov@yandex.ru>
2025-11-05 23:56:24 +0300

    тест даг

---
+++ dags/dag1.py
@@ -0,0 +1,27 @@
+from datetime import datetime, timedelta
+from airflow.models import DAG
+from airflow.operators.bash import BashOperator
+
+default_args = {
+    "owner": "airflow",
+    "depends_on_past": False,
+    "start_date": datetime(2025, 10, 26),
+    "retry_delay": timedelta(minutes=0.1),
+    }
+
+dag = DAG('get_currates',
+          default_args=default_args,
+          schedule_interval=None,
+          catchup=True,
+          max_active_tasks= 5,
+          max_active_runs=1,
+          tags=["Test", "MyFirstDag"]
+          )
+#'0 1 * * *'
+task1=BashOperator(
+    task_id='task2',
+    bash_command='python3 /airflow/scripts/dag1/task2.py',
+    dag=dag
+)
+
+task1
